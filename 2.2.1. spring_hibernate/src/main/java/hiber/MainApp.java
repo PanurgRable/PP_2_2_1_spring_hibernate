1ade41b800ee11a33dc49c713748f0d31cbfedec
PanurgRable <112486624+PanurgRable@users.noreply.github.com>
2023-01-18 15:53:11 +0300

    Update MainApp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,21 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      Car car1 = new Car("Lada", 12);
+      Car car2 = new Car("Volga", 111);
+      Car car3 = new Car("Audi", 777);
+      Car car4 = new Car("BWM", 5);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru",car1);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru",car2);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru",car3);
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru",car4);
+
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +37,10 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
+      System.out.println(userService.getUserByCar("Lada", 12));
 
       context.close();
    }
